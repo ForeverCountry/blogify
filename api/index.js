ea9678d40b0ad663b399fee9905c144d40333d34
Maxwel <40751841+honorifia@users.noreply.github.com>
2024-05-24 01:28:45 +0200

    Improve UX

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
